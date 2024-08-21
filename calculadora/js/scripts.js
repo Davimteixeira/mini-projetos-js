10f023b0fc4f975b5539a2b1ae7a1fcd8ac57c45
Davimteixeira <daviteixeira077@gmail.com>
2024-08-21 15:20:11 -0300

    utlilizando outra forma de gerar o calculo

--- calculadora/js/scripts.js
+++ calculadora/js/scripts.js
@@ -1,3 +1,51 @@
+// // Seleção de elementos
+// const multiplicationFomr = document.querySelector("#multiplication-form");
+// ;const numberInput = document.querySelector("#number")
+// const multiplicationInput = document.querySelector("#multiplicador");
+
+// const multiplicationTitle = document.querySelector("#multiplication-title span")
+
+// const multiplicationTable = document.querySelector("#multiplication-operations");
+
+// // Funções
+// const createTable = (number, multiplicatorNumber) => {
+//     multiplicationTable.innerHTML = "";
+
+//     for (i =1; i <= multiplicatorNumber; i++) {
+//         const result = number * i;
+
+//         const template = `<div class="row">
+//                  <div class="operation> ${number} x ${i} = </div>
+//                  <div class="result">${result}</div>
+//             </div>`
+        
+//         const parser = new DOMParser()
+
+//         const htmlTemplate = parser.parseFromString(template, "text/html")
+
+//         const row = htmlTemplate.querySelector(".row");
+
+//         multiplicationTable.appendChild(row);
+//     }
+//     multiplicationTitle.innerText = number;
+// }
+
+// // Enventos
+
+// multiplicationFomr.addEventListener("submit", (e) => {
+//     e.preventDefault();
+
+//     const multiplicationNumber = +numberInput.value;
+
+//     const multiplicatorNumber = +multiplicationInput.value;
+
+//     if (!multiplicationNumber || !multiplicationNumber) return;
+
+//     createTable(multiplicationNumber, multiplicatorNumber)
+// })
+
+
+
 // Seleção de elementos
 const multiplicationFomr = document.querySelector("#multiplication-form");
 ;const numberInput = document.querySelector("#number")
@@ -14,16 +62,19 @@ const createTable = (number, multiplicatorNumber) => {
     for (i =1; i <= multiplicatorNumber; i++) {
         const result = number * i;
 
-        const template = `<div class="row">
-                 <div class="operation> ${number} x ${i} = </div>
-                 <div class="result">${result}</div>
-            </div>`
-        
-        const parser = new DOMParser()
+        const row = document.createElement("div")
+        row.className = "row"
 
-        const htmlTemplate = parser.parseFromString(template, "text/html")
+        const operation = document.createElement("div")
+        operation.className = "operation"
+        operation.textContent = `${number} x ${i} = `
 
-        const row = htmlTemplate.querySelector(".row");
+
+        const result2 = document.createElement("div")
+        result2.className = "result"
+        result2.textContent = result
+
+        row.append(operation, result2)
 
         multiplicationTable.appendChild(row);
     }
